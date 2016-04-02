ed6bc18fe84fbf735717b971c7270053fca0d26e
Brett Lockhart <blockhart615@gmail.com>
2016-04-01 21:28:20 -0700

    Add DML commands to populate tables

--- AlbumProductions.sql
+++ AlbumProductions.sql
@@ -28,7 +28,19 @@ CREATE TABLE Album (
     CONSTRAINT pk_Album PRIMARY KEY (album_title, group_name)
 );
 
+INSERT INTO RecordingGroup VALUES ('Water Bottle', 'Blue', 2015, 'Liquid'),
+    ('Pencil', 'Bic', 2015, 'The Writers'), ('Whiteboard', 'Expo', 2015, 'Draw Something'); 
 
+INSERT INTO RecordingStudio VALUES ('some studio', '123 fake street', 'bill murray', '555-555-5555'),
+    ('Banded Rocking Records', '1337 maple street', 'Maple Ari', '333-333-3333');
+
+INSERT INTO Album VALUES ('New Album', '2014-06-06' , '23:59:59' , 'stinky', 'some studio', 'Whiteboard'),
+    ('Anther Album', '2014-06-06' , '23:59:59' , 'stinky', 'some studio', 'Water Bottle'), 
+    ('New Album', '2011-04-06', '10:00', 'Cool Genre', 'Banded Rocking Records', 'Pencil');
+
+Select * FROM RecordingStudio;
+Select * FROM RecordingGroup;
+Select * FROM Album;
 
 -- DROP TABLE Album;
 -- 
